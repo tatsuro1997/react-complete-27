2c64b7e38bc4ee6e3db4da154d8350803ed10815
tatsuro1997 <minagawa@dptr.jp>
2022-03-25 17:16:30 +0900

    The Context API & TypeScript

--- 27-react-ts/src/App.tsx
+++ 27-react-ts/src/App.tsx
@@ -1,31 +1,15 @@
-import { useState } from 'react';
-
 import Todos from './components/Todos';
-import Todo from './modals/todo';
 import NewTodo from './components/NewTodo';
+import TodosContextProvider from './store/todos-context';
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
 
-  const addTodoHanlder = (todoText: string) => {
-    const newTodo = new Todo(todoText);
-
-    setTodos((prevTodos) => {
-      return prevTodos.concat(newTodo);
-    });
-  };
-
-  const removeTodoHandler = (todoId: string) => {
-    setTodos((prevTodos) => {
-      return prevTodos.filter(todo => todo.id !== todoId);
-    });
-  }
 
   return (
-    <div>
-      <NewTodo onAddTodo={addTodoHanlder} />
-      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
-    </div>
+    <TodosContextProvider>
+      <NewTodo />
+      <Todos />
+    </TodosContextProvider>
   );
 }
 
